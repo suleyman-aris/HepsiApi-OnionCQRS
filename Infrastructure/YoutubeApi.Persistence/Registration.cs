bb67eb446ab612c02f3932a59cabf55b52428f15
Süleyman Ariş <suleymanaris26@gmail.com>
2025-02-14 21:28:36 +0300

    created unit of work

--- Infrastructure/YoutubeApi.Persistence/Registration.cs
+++ Infrastructure/YoutubeApi.Persistence/Registration.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YoutubeApi.Application.Interface.Repositories;
+using YoutubeApi.Application.Interface.UnitOfWorks;
 using YoutubeApi.Persistence.Context;
 using YoutubeApi.Persistence.Repositories;
+using YoutubeApi.Persistence.UnitOfWorks;
 
 namespace YoutubeApi.Persistence
 {
@@ -21,6 +23,8 @@ namespace YoutubeApi.Persistence
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
         }
     }
 }

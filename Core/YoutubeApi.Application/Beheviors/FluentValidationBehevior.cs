8f7786258063eaee3ce4f67432892c6502996cc2
Süleyman Ariş <suleymanaris26@gmail.com>
2025-02-20 17:47:31 +0300

    validation pipline

---
+++ Core/YoutubeApi.Application/Beheviors/FluentValidationBehevior.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace YoutubeApi.Application.Beheviors
+{
+    public class FluentValidationBehevior<TRequest, TResponse> : IPipelineBehavior<IRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> validator;
+
+        public FluentValidationBehevior(IEnumerable<IValidator<TRequest>> validator)
+        {
+            this.validator = validator;
+        }
+
+        public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var fialtures = validator
+                .Select(v => v.Validate(context))
+                .SelectMany(result => result.Errors)
+                .GroupBy(x => x.ErrorMessage)
+                .Select(x => x.First())
+                .Where(f => f != null)
+                .ToList();
+
+            if (fialtures.Any())
+                throw new ValidationException(fialtures);
+            return next();
+        }
+
+        public Task<TResponse> Handle(IRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/YoutubeApi.Application/Features/Products/Command/UpdateProduct/UpdateProductCommandValidator.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Application.Features.Products.Command.UpdateProduct
+{
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommandRequest>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Title)
+               .NotEmpty()
+               .WithName("Başlık");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithName("Açıklama");
+
+            RuleFor(x => x.BrandId)
+                .GreaterThan(0)
+                .WithName("Marka");
+
+            RuleFor(x => x.Price)
+               .GreaterThan(0)
+               .WithName("Fiat");
+
+            RuleFor(x => x.Discount)
+               .GreaterThanOrEqualTo(0)
+               .WithName("İndirim oranı");
+
+            RuleFor(x => x.CategoryIds)
+                .NotEmpty()
+                .Must(categories => categories.Any())
+                .WithName("Kategoriler");
+        }
+    }
+}
